d512d8923174be82dda9be2c3ab200b42cd28d21
ShogunPanda <shogun_panda@me.com>
2013-02-08 20:06:08 -0800

    WIP: Reducing complexity

--- lib/akaer/application.rb
+++ lib/akaer/application.rb
@@ -14,10 +14,193 @@ module Akaer
   #   @return [Mamertes::Command] The Mamertes command.
   # @attribute
   #   @return [Bovem::Logger] logger The logger for this application.
+
+  # Methods for the {Application Application} class.
+  module ApplicationMethods
+    # System management methods.
+    module System
+      extend ActiveSupport::Concern
+
+      # Checks if we are running on MacOS X.
+      #
+      # System services are only available on that platform.
+      #
+      # @return [Boolean] `true` if the current platform is MacOS X, `false` otherwise.
+      def is_osx?
+        ::RbConfig::CONFIG['host_os'] =~ /^darwin/
+      end
+
+      # Adds or removes an alias from the interface.
+      #
+      # @param type [Symbol] The operation to execute. Can be `:add` or `:remove`.
+      # @param address [String] The address to manage.
+      # @return [Boolean] `true` if operation succedeed, `false` otherwise.
+      def manage(type, address)
+        rv = true
+        config = self.config
+        quiet = config.quiet
+
+        command, length, index, prefix = setup_management(type, address)
+
+        # Now execute
+        if !config.dry_run then
+          log_management(prefix, type, "Removing", "Adding", address, config, quiet)
+          rv = self.execute_command(command)
+          @logger.error(@command.application.console.replace_markers("Cannot {mark=bright}#{(type == :remove ? "remove" : "add")}{/mark} address {mark=bright}#{address}{/mark} #{type != :remove ? "to" : "from"} interface {mark=bright}#{config.interface}{/mark}.")) if !rv
+        else
+          log_management(prefix, type, "remove", "add", address, config, quiet)
+        end
+
+        rv
+      end
+
+      # Adds aliases to the interface.
+      #
+      # @return [Boolean] `true` if action succedeed, `false` otherwise.
+      def action_add
+        manage_action(:add, "No valid addresses to add to the interface found.", self.config.quiet)
+      end
+
+      # Removes aliases from the interface.
+      #
+      # @return [Boolean] `true` if action succedeed, `false` otherwise.
+      def action_remove
+        manage_action(:remove, "No valid addresses to remove from the interface found.", self.config.quiet)
+      end
+
+      # Installs the application into the autolaunch.
+      #
+      # @return [Boolean] `true` if action succedeed, `false` otherwise.
+      def action_install
+        logger = get_logger
+        quiet = self.config.quiet
+
+        if !self.is_osx? then
+          logger.fatal("Install akaer on autolaunch is only available on MacOSX.") if !quiet
+          return false
+        end
+
+        launch_agent = self.launch_agent_path
+
+        begin
+          logger.info("Creating the launch agent in {mark=bright}#{launch_agent}{/mark} ...") if !quiet
+
+          args = $ARGV ? $ARGV[0, $ARGV.length - 1] : []
+
+          plist = {"KeepAlive" => false, "Label" => "it.cowtech.akaer", "Program" => (::Pathname.new(Dir.pwd) + $0).to_s, "ProgramArguments" => args, "RunAtLoad" => true}
+          ::File.open(launch_agent, "w") {|f|
+            f.write(plist.to_json)
+            f.flush
+          }
+          self.execute_command("plutil -convert binary1 \"#{launch_agent}\"")
+        rescue => e
+          logger.error("Cannot create the launch agent.") if !quiet
+          return false
+        end
+
+        begin
+          logger.info("Loading the launch agent ...") if !quiet
+          self.execute_command("launchctl load -w \"#{launch_agent}\" > /dev/null 2>&1")
+        rescue => e
+          logger.error("Cannot load the launch agent.") if !quiet
+          return false
+        end
+
+        true
+      end
+
+      # Uninstalls the application from the autolaunch.
+      #
+      # @return [Boolean] `true` if action succedeed, `false` otherwise.
+      def action_uninstall
+        logger = self.get_logger
+        quiet = self.config.quiet
+
+        if !self.is_osx? then
+          logger.fatal("Install akaer on autolaunch is only available on MacOSX.") if !quiet
+          return false
+        end
+
+        launch_agent = self.launch_agent_path
+
+        # Unload the launch agent.
+        begin
+          self.execute_command("launchctl unload -w \"#{launch_agent}\" > /dev/null 2>&1")
+        rescue => e
+          logger.warn("Cannot unload the launch agent.") if !quiet
+        end
+
+        # Delete the launch agent.
+        begin
+          logger.info("Deleting the launch agent #{launch_agent} ...")
+          ::File.delete(launch_agent)
+        rescue => e
+          logger.warn("Cannot delete the launch agent.") if !quiet
+          return false
+        end
+
+        true
+      end
+
+
+      private
+        # Setup management.
+        #
+        # @param type [Symbol] The type of operation. Can be `:add` or `:remove`.
+        # @param address [String] The address to manage.
+        # @return [Array] A list of parameters for the management.
+        def setup_management(type, address)
+          # Compute the command
+          command = config.send((type == :remove) ? :remove_command : :add_command).gsub("@INTERFACE@", config.interface).gsub("@ALIAS@", address) + " > /dev/null 2>&1"
+
+          # Compute the prefix
+          @addresses ||= self.compute_addresses
+          length = self.pad_number(@addresses.length)
+          index = (@addresses.index(address) || 0) + 1
+          prefix = "{mark=blue}[{mark=bright white}#{self.pad_number(index, length.length)}{mark=reset blue}/{/mark}#{length}{/mark}]{/mark}"
+
+          [command, length, index, prefix]
+        end
+
+        # Logs an operation.
+        #
+        # @param prefix [String] The prefix to apply to the message.
+        # @param type [Symbol] The type of operation. Can be `:add` or `:remove`.
+        # @param remove_label [String] The label to use for removing.
+        # @param add_label [String] The label to use for adding.
+        # @param address [String] The address that will be managed.
+        # @param config [Configuration] The current configuration.
+        # @param quiet [Boolean] Whether to show the message.
+        def log_management(prefix, type, remove_label, add_label, address, config, quiet)
+          @logger.info(@command.application.console.replace_markers("#{prefix} I will {mark=bright}#{(type == :remove ? remove_label : add_label)}{/mark} address {mark=bright}#{address}{/mark} #{type != :remove ? "to" : "from"} interface {mark=bright}#{config.interface}{/mark}...")) if !quiet
+        end
+
+        # Manages an action on the request addresses.
+        #
+        # @param operation [Symbol] The type of operation. Can be `:add` or `:remove`.
+        # @param message [String] The message to show if no addresses are found.
+        # @param quiet [Boolean] Whether to show the failure message.
+        def manage_action(operation, message, quiet)
+          addresses = self.compute_addresses
+
+          if addresses.present? then
+            # Now, for every address, call the command
+            addresses.all? {|address|
+              self.manage(operation, address)
+            }
+          else
+            @logger.error(message) if !quiet
+          end
+        end
+    end
+  end
+
   class Application
     attr_reader :config
     attr_reader :command
     attr_accessor :logger
+
+    include Akaer::ApplicationMethods::System
 
     # Creates a new application.
     #
@@ -55,15 +238,6 @@ module Akaer
       end
 
       self
-    end
-
-    # Checks if we are running on MacOS X.
-    #
-    # System services are only available on that platform.
-    #
-    # @return [Boolean] `true` if the current platform is MacOS X, `false` otherwise.
-    def is_osx?
-      ::RbConfig::CONFIG['host_os'] =~ /^darwin/
     end
 
     # Checks if and address is a valid IPv4 address.
@@ -163,127 +337,6 @@ module Akaer
       rv
     end
 
-    # Adds or removes an alias from the interface.
-    #
-    # @param type [Symbol] The operation to execute. Can be `:add` or `:remove`.
-    # @param address [String] The address to manage.
-    # @return [Boolean] `true` if operation succedeed, `false` otherwise.
-    def manage(type, address)
-      rv = true
-      config = self.config
-      quiet = config.quiet
-      # Compute the command
-      command = config.send((type == :remove) ? :remove_command : :add_command)
-
-      # Interpolate
-      command = command.gsub("@INTERFACE@", config.interface).gsub("@ALIAS@", address) + " > /dev/null 2>&1"
-
-      # Compute the prefix
-      @addresses ||= self.compute_addresses
-      length = self.pad_number(@addresses.length)
-      index = (@addresses.index(address) || 0) + 1
-      prefix = "{mark=blue}[{mark=bright white}#{self.pad_number(index, length.length)}{mark=reset blue}/{/mark}#{length}{/mark}]{/mark}"
-
-      # Now execute
-      if !config.dry_run then
-        log_management(prefix, type, "Removing", "Adding", address, config, quiet)
-        rv = self.execute_command(command)
-        @logger.error(@command.application.console.replace_markers("Cannot {mark=bright}#{(type == :remove ? "remove" : "add")}{/mark} address {mark=bright}#{address}{/mark} #{type != :remove ? "to" : "from"} interface {mark=bright}#{config.interface}{/mark}.")) if !rv
-      else
-        log_management(prefix, type, "remove", "add", address, config, quiet)
-      end
-
-      rv
-    end
-
-    # Adds aliases to the interface.
-    #
-    # @return [Boolean] `true` if action succedeed, `false` otherwise.
-    def action_add
-      manage_action(:add, "No valid addresses to add to the interface found.", self.config.quiet)
-    end
-
-    # Removes aliases from the interface.
-    #
-    # @return [Boolean] `true` if action succedeed, `false` otherwise.
-    def action_remove
-      manage_action(:remove, "No valid addresses to remove from the interface found.", self.config.quiet)
-    end
-
-    # Installs the application into the autolaunch.
-    #
-    # @return [Boolean] `true` if action succedeed, `false` otherwise.
-    def action_install
-      logger = get_logger
-      quiet = self.config.quiet
-
-      if !self.is_osx? then
-        logger.fatal("Install akaer on autolaunch is only available on MacOSX.") if !quiet
-        return false
-      end
-
-      launch_agent = self.launch_agent_path
-
-      begin
-        logger.info("Creating the launch agent in {mark=bright}#{launch_agent}{/mark} ...") if !quiet
-
-        args = $ARGV ? $ARGV[0, $ARGV.length - 1] : []
-
-        plist = {"KeepAlive" => false, "Label" => "it.cowtech.akaer", "Program" => (::Pathname.new(Dir.pwd) + $0).to_s, "ProgramArguments" => args, "RunAtLoad" => true}
-        ::File.open(launch_agent, "w") {|f|
-          f.write(plist.to_json)
-          f.flush
-        }
-        self.execute_command("plutil -convert binary1 \"#{launch_agent}\"")
-      rescue => e
-        logger.error("Cannot create the launch agent.") if !quiet
-        return false
-      end
-
-      begin
-        logger.info("Loading the launch agent ...") if !quiet
-        self.execute_command("launchctl load -w \"#{launch_agent}\" > /dev/null 2>&1")
-      rescue => e
-        logger.error("Cannot load the launch agent.") if !quiet
-        return false
-      end
-
-      true
-    end
-
-    # Uninstalls the application from the autolaunch.
-    #
-    # @return [Boolean] `true` if action succedeed, `false` otherwise.
-    def action_uninstall
-      logger = self.get_logger
-      quiet = self.config.quiet
-
-      if !self.is_osx? then
-        logger.fatal("Install akaer on autolaunch is only available on MacOSX.") if !quiet
-        return false
-      end
-
-      launch_agent = self.launch_agent_path
-
-      # Unload the launch agent.
-      begin
-        self.execute_command("launchctl unload -w \"#{launch_agent}\" > /dev/null 2>&1")
-      rescue => e
-        logger.warn("Cannot unload the launch agent.") if !quiet
-      end
-
-      # Delete the launch agent.
-      begin
-        logger.info("Deleting the launch agent #{launch_agent} ...")
-        ::File.delete(launch_agent)
-      rescue => e
-        logger.warn("Cannot delete the launch agent.") if !quiet
-        return false
-      end
-
-      true
-    end
-
     # Returns a unique (singleton) instance of the application.
     #
     # @param command [Mamertes::Command] The current Mamertes command.
@@ -293,37 +346,5 @@ module Akaer
       @instance = nil if force
       @instance ||= Akaer::Application.new(command)
     end
-
-    private
-      # Logs an operation.
-      #
-      # @param prefix [String] The prefix to apply to the message.
-      # @param type [Symbol] The type of operation. Can be `:add` or `:remove`.
-      # @param remove_label [String] The label to use for removing.
-      # @param add_label [String] The label to use for adding.
-      # @param address [String] The address that will be managed.
-      # @param config [Configuration] The current configuration.
-      # @param quiet [Boolean] Whether to show the message.
-      def log_management(prefix, type, remove_label, add_label, address, config, quiet)
-        @logger.info(@command.application.console.replace_markers("#{prefix} I will {mark=bright}#{(type == :remove ? remove_label : add_label)}{/mark} address {mark=bright}#{address}{/mark} #{type != :remove ? "to" : "from"} interface {mark=bright}#{config.interface}{/mark}...")) if !quiet
-      end
-
-      # Manages an action on the request addresses.
-      #
-      # @param operation [Symbol] The type of operation. Can be `:add` or `:remove`.
-      # @param message [String] The message to show if no addresses are found.
-      # @param quiet [Boolean] Whether to show the failure message.
-      def manage_action(operation, message, quiet)
-        addresses = self.compute_addresses
-
-        if addresses.present? then
-          # Now, for every address, call the command
-          addresses.all? {|address|
-            self.manage(operation, address)
-          }
-        else
-          @logger.error(message) if !quiet
-        end
-      end
   end
 end
